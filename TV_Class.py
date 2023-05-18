b0729051c3c0ee71edd9ff74589f26de6984b042
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-18 22:29:59 +0800

    Pseudocode

---
+++ TV_Class.py
@@ -0,0 +1,17 @@
+# Create a TV class with the stated attributes
+
+    # Create methods for the default constructor
+
+    # Method for turning the tv on or off
+
+    # A method for getting the channel
+        # Conditional statements for the amount of channels available
+
+    # Method for getting the volume
+        # Conditional statements for the range of volume
+
+    # Method for chagning the channel
+        # Conditional statements for the amount of channels available
+   
+    # Method for chagning the volume
+        # Conditional statements for the range of volume
